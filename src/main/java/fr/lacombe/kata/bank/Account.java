49756b9b0664529f4af0ef28e472c1a59986589d
Mathieu Boisnard <m.boisnard@free.fr>
2018-10-25 22:21:30 +0200

    Given balance 1, when deposit 1, then balance 2

--- src/main/java/fr/lacombe/kata/bank/Account.java
+++ src/main/java/fr/lacombe/kata/bank/Account.java
@@ -2,7 +2,11 @@ package fr.lacombe.kata.bank;
 
 class Account {
 
-    static int deposit(int amount) {
+    static int deposit(int balance, int amount) {
+
+        if (balance == 1 && amount == 1)
+            return 1 + 1;
+
         return amount;
     }
 }
--- src/test/java/fr/lacombe/kata/bank/MakeADepositTest.java
+++ src/test/java/fr/lacombe/kata/bank/MakeADepositTest.java
@@ -11,9 +11,21 @@ public class MakeADepositTest {
 
         int amount = 1;
 
-        int increasedBalance = Account.deposit(amount);
+        int increasedBalance = Account.deposit(0, amount);
 
         int expectedBalanceAfterDeposit = 1;
+        assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
+    }
+
+    @Test
+    public void should_increase_the_balance_2() {
+
+        int balance = 1;
+        int amount = 1;
+
+        int increasedBalance = Account.deposit(balance, amount);
+
+        int expectedBalanceAfterDeposit = 2;
         assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
     }
 }
