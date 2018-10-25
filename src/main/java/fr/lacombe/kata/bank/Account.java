58cb8d4e29588cd9af41788411e22f80e2b17760
Mathieu Boisnard <m.boisnard@free.fr>
2018-10-25 22:30:04 +0200

    Refactoring deposit with balance

--- src/main/java/fr/lacombe/kata/bank/Account.java
+++ src/main/java/fr/lacombe/kata/bank/Account.java
@@ -3,14 +3,6 @@ package fr.lacombe.kata.bank;
 class Account {
 
     static int deposit(int balance, int amount) {
-
-        if (balance == 3 && amount == 1)
-            return 3 + 1;
-        if (balance == 2 && amount == 1)
-            return 2 + 1;
-        if (balance == 1 && amount == 1)
-            return 1 + 1;
-
-        return 0 + amount;
+        return balance + amount;
     }
 }
--- src/test/java/fr/lacombe/kata/bank/MakeADepositTest.java
+++ src/test/java/fr/lacombe/kata/bank/MakeADepositTest.java
@@ -9,47 +9,12 @@ public class MakeADepositTest {
     @Test
     public void should_increase_the_balance() {
 
-        int amount = 1;
-
-        int increasedBalance = Account.deposit(0, amount);
-
-        int expectedBalanceAfterDeposit = 1;
-        assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
-    }
-
-    @Test
-    public void should_increase_the_balance_2() {
-
         int balance = 1;
         int amount = 1;
 
         int increasedBalance = Account.deposit(balance, amount);
 
         int expectedBalanceAfterDeposit = 2;
-        assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
-    }
-
-    @Test
-    public void should_increase_the_balance_3() {
-
-        int balance = 2;
-        int amount = 1;
-
-        int increasedBalance = Account.deposit(balance, amount);
-
-        int expectedBalanceAfterDeposit = 3;
-        assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
-    }
-
-    @Test
-    public void should_increase_the_balance_4() {
-
-        int balance = 3;
-        int amount = 1;
-
-        int increasedBalance = Account.deposit(balance, amount);
-
-        int expectedBalanceAfterDeposit = 4;
         assertThat(increasedBalance).isEqualTo(expectedBalanceAfterDeposit);
     }
 }
