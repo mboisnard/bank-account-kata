a3db90747dba210e8174f244a6dde3ef5d15915d
Mathieu Boisnard <m.boisnard@free.fr>
2018-10-27 13:57:18 +0200

    Refactoring tests

--- src/test/java/fr/lacombe/kata/bank/AmountTest.java
+++ src/test/java/fr/lacombe/kata/bank/AmountTest.java
@@ -17,29 +17,33 @@ public class AmountTest {
         Amount amount = Amount.of(1);
         Amount amountToAdd = Amount.of(1);
 
-        Amount amountAfterAdd = amount.add(amountToAdd);
+        Amount addedAmount = amount.add(amountToAdd);
 
         Amount expectedAddedAmount = Amount.of(2);
-        assertThat(amountAfterAdd).isEqualTo(expectedAddedAmount);
+        assertThat(addedAmount).isEqualTo(expectedAddedAmount);
     }
 
     @Test
     public void should_subtract_amount_with_another_amount() {
 
         Amount amount = Amount.of(2);
-        Amount amountAfterSubtract = amount.subtract(Amount.of(1));
+        Amount amountToSubtract = Amount.of(1);
+
+        Amount subtractedAmount = amount.subtract(amountToSubtract);
 
         Amount expectedSubtractAmount = Amount.of(1);
-        assertThat(amountAfterSubtract).isEqualTo(expectedSubtractAmount);
+        assertThat(subtractedAmount).isEqualTo(expectedSubtractAmount);
     }
 
     @Test
     public void should_subtract_amount_with_another_amount_2() {
 
         Amount amount = Amount.of(5);
-        Amount amountAfterSubtract = amount.subtract(Amount.of(2));
+        Amount amountToSubtract = Amount.of(2);
+
+        Amount subtractedAmount = amount.subtract(amountToSubtract);
 
         Amount expectedSubtractAmount = Amount.of(3);
-        assertThat(amountAfterSubtract).isEqualTo(expectedSubtractAmount);
+        assertThat(subtractedAmount).isEqualTo(expectedSubtractAmount);
     }
 }
