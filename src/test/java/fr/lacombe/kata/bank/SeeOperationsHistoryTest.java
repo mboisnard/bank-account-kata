f112275a51e52d8a67fa2c244e04ab3c08d7ab67
Mathieu Boisnard <m.boisnard@free.fr>
2018-10-28 10:53:09 +0100

    Improve acceptance test after introduce amount and balance in operation

--- src/test/java/fr/lacombe/kata/bank/SeeOperationsHistoryTest.java
+++ src/test/java/fr/lacombe/kata/bank/SeeOperationsHistoryTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static fr.lacombe.kata.bank.OperationType.DEPOSIT;
+import static fr.lacombe.kata.bank.OperationType.WITHDRAW;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SeeOperationsHistoryTest {
@@ -23,5 +25,10 @@ public class SeeOperationsHistoryTest {
 
         int nbOperations = 5;
         assertThat(history).hasSize(nbOperations);
+        assertThat(history.get(0)).isEqualTo(Operation.of(DEPOSIT, Amount.of(50), Amount.of(150)));
+        assertThat(history.get(1)).isEqualTo(Operation.of(WITHDRAW, Amount.of(10), Amount.of(140)));
+        assertThat(history.get(2)).isEqualTo(Operation.of(DEPOSIT, Amount.of(50), Amount.of(190)));
+        assertThat(history.get(3)).isEqualTo(Operation.of(WITHDRAW, Amount.of(20), Amount.of(170)));
+        assertThat(history.get(4)).isEqualTo(Operation.of(DEPOSIT, Amount.of(100), Amount.of(270)));
     }
 }
